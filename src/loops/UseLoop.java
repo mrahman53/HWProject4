bda4cccf4baadd477d92155bc5178bcba4c90b22
mrahman <mohammad.rahman@hbo.com>
2014-11-18 11:42:16 -0500

    minor change

--- src/loops/UseLoop.java
+++ src/loops/UseLoop.java
@@ -3,7 +3,14 @@ package loops;
 public class UseLoop {
 
 	public static void main(String[] args) {
-		/* Write an for loop to     */
+		/* Write a for loop to store numbers into an Array, Then use another for loop to retrieve 
+		 *  the numbers from the Array and print to console.
+		 *
+		 *  Hints: Use Scanner class with System.in constructor to get input from keyboard.
+		 *
+		 *
+		 *
+		 */
 		
 		
 		
